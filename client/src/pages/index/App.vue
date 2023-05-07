b0d648ea615b579bf6aeaf7c12c0b5e3266ec6cc
Hotshot824 <shen485@gmail.com>
2023-05-07 02:53:21 +0000

    add: file structure.

---
+++ client/src/pages/index/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Navbar from './components/Navbar.vue'
+import Sidebar from './components/Sidebar.vue'
+import DHTSensor from './components/DHTSensor.vue'
+</script>
+
+<template>
+  <Navbar />
+  <DHTSensor msg="DHT22"/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
